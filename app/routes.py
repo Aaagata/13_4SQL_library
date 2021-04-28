f116b4bd9a272a58d936f8fedba90aeea21a0d6f
Agata Dunaj <a.dunaj@yahoo.co.uk>
2021-04-28 10:14:06 +0200

    init the project

---
+++ app/routes.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, render_template, redirect
+from models import Band, Video
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+from app import app, db
+#importy niby na początku, ale... ten 'myk' zapobiega circular imports
+
+@app.route("/")
+def video_list():
+    videos = Video.query.all()
+    bands = Band.query.all()
+    return render_template("videos.html", videos=videos, bands=bands)
+
+@app.route("/video/", methods=["POST"])
+def add_video():
+    title = request.form["title"]
+    genre = request.form["genre"]
+    songs = request.form["songs"]
+    if not title:
+        return "Error"
+
+    video = Video(title=title, genre=genre, songs=songs)
+    db.session.add(video)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/band/", methods=["POST"])
+def add_band():
+    band = request.form["band"]
+    formation = request.form ["formation"]
+    if not name:
+        return "Error"
+
+    band = Band(band=band, formation=formation)
+    db.session.add(band)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/assign/", methods=["POST"])
+def assign_video():
+    video_id = request.form["video_id"]
+    band_id = request.form["band__id"]
+
+    video = Video.query.get(video_id)
+    video.band_id = band_id
+
+    db.session.add(video)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/delete/<int:video_id>")
+def delete_video(video_id):
+    video = Video.query.get(video_id)
+    if not video:
+        return redirect("/")
+
+    db.session.delete(video)
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/delete/<int:band_id>")
+def delete_author(band_id):
+
+    band = Band.query.get(band_id)
+    if not band:
+        return redirect("/")
+
+    db.session.delete(band)
+    db.session.commit()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
